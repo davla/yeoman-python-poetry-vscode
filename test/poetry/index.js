784379c8754511489577e5f099f5c9db355a0cee
Davide Laezza <truzzialrogo@gmx.com>
2023-01-24 18:20:19 +0100

    test(poetry): use mocha context

--- test/poetry/index.js
+++ test/poetry/index.js
@@ -73,21 +73,18 @@ const mandatoryAnswers = {
 };
 
 describe("python-poetry-vscode:poetry", () => {
-  let generator;
-  let stubs;
-
-  beforeEach(() => {
-    stubs = setupSystemAccessStubs();
-    stubs.queryGitOriginUrl.resolves(
+  beforeEach(function () {
+    this.stubs = setupSystemAccessStubs();
+    this.stubs.queryGitOriginUrl.resolves(
       "https://github.com/eddy-gordo/git_package"
     );
-    stubs.spawnCommand
+    this.stubs.spawnCommand
       .withArgs("python", ["--version"], { stdio: "pipe" })
       .resolves({ stdout: "Python 3.10.2" });
-    stubs.userGitEmail.returns("jin.kazama@tekken.jp");
-    stubs.userGitName.returns("Jin Kazama");
+    this.stubs.userGitEmail.returns("jin.kazama@tekken.jp");
+    this.stubs.userGitName.returns("Jin Kazama");
 
-    generator = yeomanTest
+    this.generator = yeomanTest
       .run(PoetryGenerator)
       // Silence the annoying warnings
       .withAnswers(mandatoryAnswers);
@@ -96,19 +93,20 @@ describe("python-poetry-vscode:poetry", () => {
   afterEach(cleanupSystemAccessStubs);
 
   describe("pyproject.toml", () => {
-    it("should populate pyproject.toml", async () =>
-      (await pyProjectToml(await generator)).should.matchSnapshot());
+    it("should populate pyproject.toml", async function () {
+      return (await pyProjectToml(await this.generator)).should.matchSnapshot();
+    });
 
-    it("creates the file in toml format", async () => {
-      const runResult = await generator;
+    it("creates the file in toml format", async function () {
+      const runResult = await this.generator;
       runResult.assertFile("pyproject.toml");
       await readToml(runResult, "pyproject.toml").should.be.fulfilled;
     });
 
-    it("merges with existing content", async () => {
-      const runResult = await generator
+    it("merges with existing content", async function () {
+      const runResult = await this.generator
         .doInDir(
-          writePyProjectToml.bind(generator, {
+          writePyProjectToml.bind(this.generator, {
             tool: {
               poetry: {
                 authors: ["King <king@tekken.mx>", "Jack <jack@tekken.ru"],
@@ -131,12 +129,13 @@ describe("python-poetry-vscode:poetry", () => {
       });
     });
 
-    it('adds the "build-system" section', async () =>
-      (await pyProjectToml(await generator)).should.containSubset(
+    it('adds the "build-system" section', async function () {
+      return (await pyProjectToml(await this.generator)).should.containSubset(
         PoetryGenerator.buildSystem
-      ));
+      );
+    });
 
-    it('leaves existing "build-system" sections untouched', async () => {
+    it('leaves existing "build-system" sections untouched', async function () {
       /*
        * Use an object as "build-system" value to verify it's not merged with
        * the default value, that is itself an object.
@@ -146,8 +145,8 @@ describe("python-poetry-vscode:poetry", () => {
           requires: ["setuptools", "wheel"],
         },
       };
-      const runResult = await generator.doInDir(
-        writePyProjectToml.bind(generator, existingBuildSystem)
+      const runResult = await this.generator.doInDir(
+        writePyProjectToml.bind(this.generator, existingBuildSystem)
       );
       (await pyProjectToml(runResult)).should.have
         .property("build-system")
@@ -158,19 +157,18 @@ describe("python-poetry-vscode:poetry", () => {
   describe("input", () => {
     for (const inputTestData of generatorInput) {
       const { optionName, inputValue, outputPath } = inputTestData;
-      it(`should output input "${optionName}" at "tool.poetry.${outputPath}"`, async () => {
+      it(`should output input "${optionName}" at "tool.poetry.${outputPath}"`, async function () {
         const expectedContent = inToolPoetry(outputPath, inputValue);
-        const runResult = await withInput(generator, inputTestData);
+        const runResult = await withInput(this.generator, inputTestData);
         (await pyProjectToml(runResult)).should.containSubset(expectedContent);
       });
     }
   });
 
   describe("dynamic default values", () => {
-    it("queries current python version for default python", async () => {
-      const runResult = await generator;
-
-      stubs.spawnCommand.should.have.been.calledOnceWith(
+    it("queries current python version for default python", async function () {
+      const runResult = await this.generator;
+      this.stubs.spawnCommand.should.have.been.calledOnceWith(
         "python",
         ["--version"],
         {
@@ -185,9 +183,11 @@ describe("python-poetry-vscode:poetry", () => {
   });
 
   describe("install", () => {
-    it("doesn't run poetry install", async () => {
-      await generator;
-      stubs.spawnCommand.should.not.have.been.calledWith("poetry", ["install"]);
+    it("doesn't run poetry install", async function () {
+      await this.generator;
+      this.stubs.spawnCommand.should.not.have.been.calledWith("poetry", [
+        "install",
+      ]);
     });
   });
 });
